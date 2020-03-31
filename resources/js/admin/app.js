5ad69753f6c0a9e2ae1dc7578a07d3f821f129cf
slieman <slieman2001@hotmail.com>
2020-03-31 10:46:32 +0300

    install firebase and add config to app.js

--- resources/js/admin/app.js
+++ resources/js/admin/app.js
@@ -3,7 +3,19 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-Vue.config.productionTip = false;
+import firebase from 'firebase';
+
+var config =  {
+  apiKey: "AIzaSyCxIaf6Joaf3ReO1eYnMhY0IG4DDc-NwCA",
+  authDomain: "vue-chatapp-ed381.firebaseapp.com",
+  databaseURL: "https://vue-chatapp-ed381.firebaseio.com",
+  projectId: "vue-chatapp-ed381",
+  storageBucket: "vue-chatapp-ed381.appspot.com",
+  messagingSenderId: "333394129790",
+  appId: "1:333394129790:web:f7629434fa42549527792f"
+};
+
+firebase.initalizeApp(config);
 
 new Vue({
   router,
