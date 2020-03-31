626afba485264a5565b3455ff9d54e664a725b7a
slieman <slieman2001@hotmail.com>
2020-03-31 11:35:17 +0300

    add login register pages.

--- resources/js/admin/router/routes.js
+++ resources/js/admin/router/routes.js
@@ -1,7 +1,11 @@
 import Home from "../views/Home";
-import About from "../views/About";
+import Chat from "../views/Chat";
+import Login from "../views/Login";
+import Register from "../views/Register";
 
 export default  [
     { path: '/', component: Home },
-    { path: '/about', component: About },
+    { path: '/login', component: Login },
+    { path: '/register', component: Register },
+    { path: '/chat', component: Chat },
 ]
